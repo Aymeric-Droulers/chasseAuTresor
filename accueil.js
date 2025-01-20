a5a985a113bed3dc73577c93fea6b7cb124e99b8
nathanbayard <nathan.bayard@student.junia.com>
2025-01-20 12:05:40 +0100

    redirection quand on se connecte/créé un compte

--- accueil.js
+++ accueil.js
@@ -1,5 +1,20 @@
-var Id = document.getElementById("Id");
-var Password = document.getElementById("Password");
-var Mail = document.getElementById("Mail");
 var Connect = document.getElementById("Connect");
 var Signup = document.getElementById("Signup");
+Signup.addEventListener("click",DoSignup);
+Connect.addEventListener("click",DoConnect);
+
+function DoSignup(){
+    //Vérifier que le compte n'existe pas
+    var Id = document.getElementById("Id");
+    var Password = document.getElementById("Password");
+    var Mail = document.getElementById("Mail");
+    window.location.assign('defaut.html');
+}
+
+function DoConnect(){
+    //Vérifier que le compte existe
+    var Id = document.getElementById("Id");
+    var Password = document.getElementById("Password");
+    var Mail = document.getElementById("Mail");
+    window.location.assign('defaut.html');
+}
