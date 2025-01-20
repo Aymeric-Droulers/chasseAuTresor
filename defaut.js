178883a96527ad56efae6a3a41c17110d75d74bf
nathanbayard <nathan.bayard@student.junia.com>
2025-01-20 15:48:29 +0100

    tentative d'utilisation de la db + redirection sur la page html de Charles
    Je n'ai pas réussi à faire ce que je voulais faire mais je push avant que tout parte en cacahuètes.

--- defaut.js
+++ defaut.js
@@ -2,3 +2,8 @@ var Create = document.getElementById("Create");
 var Code = document.getElementById("Code");
 var Join = document.getElementById("Join");
 var History = document.getElementById("History");
+
+Create.addEventListener("click",DoCreate);
+function DoCreate(){
+    window.location.assign('admin.html');
+}
