d55d90fedab7300d382befde068cfedc8ba0660f
nathanbayard <nathan.bayard@student.junia.com>
2025-01-20 11:33:00 +0100

    début des fichiers js, récupération des variables

---
+++ accueil.js
@@ -0,0 +1,5 @@
+var Id = document.getElementById("Id");
+var Password = document.getElementById("Password");
+var Mail = document.getElementById("Mail");
+var Connect = document.getElementById("Connect");
+var Signup = document.getElementById("Signup");
---
+++ defaut.js
@@ -0,0 +1,4 @@
+var Create = document.getElementById("Create");
+var Code = document.getElementById("Code");
+var Join = document.getElementById("Join");
+var History = document.getElementById("History");
