802a97d3d449719d4e171be4f148521cbae8bc83
Aymeric-Droulers <112798634+Aymeric-Droulers@users.noreply.github.com>
2025-01-21 10:30:41 +0100

    API getAllChasses

--- backend/src/controllers/chasseControllers.js
+++ backend/src/controllers/chasseControllers.js
@@ -1,2 +1,12 @@
 const { getDB } = require('../config/db');
 
+exports.getAllChasses = async (req, res) => {
+
+    try {
+        const DB = await getDB();
+        const chasses = await DB.collection('Chasses').find().toArray();
+        res.status(200).json(chasses);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+};
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers,getUserById,addAccount} = require('../controllers/userControllers');
-
+const {getAllChasses}=require('../controllers/chasseControllers');
 
 //  /api/users
 router.get('/accounts', getAllUsers);
 router.post('/accounts/addAccount', addAccount);
 router.get('/accounts/:id', getUserById);
 
+//   /api/chasses
 
+router.get('/chasses',getAllChasses);
 
 module.exports = router;
